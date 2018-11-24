bb122c0ce9dca69d75c364727ef18274aa57c94f
MaltaK <mariakolobaeva0369@gmail.com>
2018-11-24 12:04:18 +0300

    описание

--- hello.c
+++ hello.c
@@ -5,12 +5,12 @@
 
 static int __init hello_init(void)
 {
-	pr_alert("init\n");
+	pr_alert("initt\n");
 	return 0;
 }
 static void __exit hello_exit(void)
 {
-	pr_alert("exit\n");
+	pr_alert("exitt\n");
 }
 module_init(hello_init);
 module_exit(hello_exit);
